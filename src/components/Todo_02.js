9f927b38b5510ad5b573f37f946ac944f63f6dec
zous71628 <jack71628@gmail.com>
2021-10-07 19:49:10 +0800

    save

--- src/components/Todo_02.js
+++ src/components/Todo_02.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Backdrop_02 from './Backdrop_02';
+import Modal_02 from './Modal_02';
 
 function Todo(props) {
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler() {
+    setShowModal(true);
+  }
+
+  function closeModalHandler() {
+    setShowModal(false);
+  }
   return (
     <div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">{props.text1}</button>
+          <button className="btn" onClick={showModalHandler}>
+            {props.text1}
+          </button>
         </div>
       </div>
+      {showModal && <Backdrop_02 onClose={closeModalHandler}/>}
+      {showModal && <Modal_02 text="Are you sure?" onClose={closeModalHandler} />}
     </div>
   );
 }
